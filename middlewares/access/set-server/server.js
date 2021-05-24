d171d3e8cfa400c838d7fe24c7cd65902982be3c
SorinGFS <sorin.gfs@protonmail.com>
2021-05-24 18:02:00 +0300

    fix: throw error if no server

--- middlewares/access/set-server/server.js
+++ middlewares/access/set-server/server.js
@@ -7,6 +7,7 @@ function setServer(req, res, next) {
     req.performer = 'setServer';
     // using the serialized serverName in base servers
     req.server = servers[Object.keys(servers).filter((name) => new RegExp(fn.btoa(`<${req.hostname}>`)).test(name))[0]];
+    if (!req.server) throw new Error(`There is no configured serverName that matches ${req.hostname}`);
     // combine server and location rules
     if (req.server.locations) {
         req.server.locations.some((location) => {
