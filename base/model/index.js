a85bfc6b834e9da28a25f7e30a4ae6781fe629bf
SorinGFS <sorin.gfs@protonmail.com>
2021-07-28 11:26:03 +0300

    update mongodb model index

--- base/model/index.js
+++ base/model/index.js
@@ -1,8 +1,8 @@
 'use strict';
 // dynamic model is connector specific, custom model is extending base model and is dbName specific
 // connection object must contain all the connector specific needs
-module.exports = (connection) => {
-    // console.log(connection);
+module.exports = (namespace) => {
+    const connection = require('../connections')(namespace);
     const BaseModel = require(`./${connection.connector}`);
     class Model extends BaseModel {
         constructor(connection) {
