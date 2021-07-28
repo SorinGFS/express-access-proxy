eb19efdd9ccf28884cd43912a5bde25b6d1e5566
SorinGFS <sorin.gfs@protonmail.com>
2021-07-28 11:15:58 +0300

    mongodb connector update

--- base/connections/index.js
+++ base/connections/index.js
@@ -1,11 +1,10 @@
 'use strict';
 // this returns the connection object used by dynamic model to connect
-module.exports = (query) => {
-    const { dbName, ...params } = query;
-    const filterDbName = (config) => (config.dbName = dbName);
+module.exports = (namespace) => {
+    const filterDbName = (config) => (config.dbName = namespace.split('.')[0]);
     const connection = require('../../config/connections')(filterDbName)[0];
     const filterConnector = (config) => (config.connector = connection.connector);
     const connector = require('../../config/connectors')(filterConnector)[0];
-    const args = Object.assign(connection, connector, params);
+    const args = Object.assign(connection, connector, { namespace: namespace });
     return require(`./${connection.connector}`)(args);
 };
