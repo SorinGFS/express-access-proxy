3d86e84f233c3fa5b60f49f1d410eb8bb94207db
SorinGFS <sorin.gfs@protonmail.com>
2021-10-04 16:58:03 +0300

    add url rewrite support

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 const env = require('./config/env');
 const app = require('express')();
 const fn = require('express-access-proxy-base/fn');
-const filter = () => true; // filter all
-const configs = require('./config/servers')(filter);
+const authDb = require('./config/connections')((config) => config.dbName === 'access')[0];
+const configs = require('./config/servers')(() => true); // filter all
 const servers = require('express-access-proxy-base/servers')(configs);
+const httpParsers = require('./server/middlewares/http-parsers');
 const server = require('./server');
 // set port
 const PORT = process.env.PORT || 7331;
@@ -16,32 +17,19 @@ function setServer(req, res, next) {
     // using the serialized serverName in base servers
     req.server = servers[Object.keys(servers).filter((name) => new RegExp(fn.btoa(`<${req.hostname}>`)).test(name))[0]];
     if (!req.server) throw new Error(`There is no configured serverName that matches ${req.hostname}`);
-    // combine server and location rules
-    if (req.server.locations) {
-        req.server.locations.some((location) => {
-            Object.keys(location).some((path) => {
-                if (new RegExp(path).test(req.path)) {
-                    req.server = fn.mergeDeep({}, req.server, location[path]);
-                }
-            });
-        });
-    }
-    // app settings
-    if (req.server.appSettings) {
-        Object.keys(req.server.appSettings).forEach((key) => {
-            app.set(key, req.server.appSettings[key]);
-        });
-    }
-    // access db connection (if auth is set)
-    if (req.server.auth && req.server.auth.mode) {
-        const connection = require('./config/connections')((config) => config.dbName === 'access')[0];
-        if (!connection) throw new Error(`Error: <access> db connection config not found!`);
-        connection.namespace = 'access.permissions';
-        req.server.Permissions = require('express-access-proxy-base/db/model')(connection);
-    }
+    // rewrite at server level
+    if (req.server.urlRewrite) req.server.rewrite(req, req.server.urlRewrite);
+    // parse locations (also rewrite at location level if any)
+    req.server.parseLocations(req);
+    // send direct response if this resulted from the parsed configuration
+    if (req.server.response.status) return req.server.send(req, res);
+    // apply app settings after the server has been combined with the location config
+    if (req.server.appSettings) app.set(req.server.appSettings);
+    // set auth db connection (for the access permissions managed by this app).
+    if (req.server.auth && req.server.auth.mode) req.server.setAuthDb(req, authDb) 
     next();
 }
 // load the settings then pass the request to main server router
-app.use(setServer, server);
+app.use(httpParsers, setServer, server);
 // in an uncertain future we may run each server in a separate process by passing its config to this app
 module.exports = app;
