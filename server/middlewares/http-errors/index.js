11032573a48b80c61ad4ffa451d0a709275d25c5
SorinGFS <sorin.gfs@protonmail.com>
2021-10-07 00:46:10 +0300

    minor change the mongodb connection object

--- server/middlewares/http-errors/index.js
+++ server/middlewares/http-errors/index.js
@@ -4,7 +4,7 @@ const handleError = (err, req, res, next) => {
     const { statusCode = 500, expose, message, stack, headers, ...rest } = err;
     if (headers) res.set(headers);
     if (!expose && message) console.log(message, stack);
-    if (!expose || statusCode < 400) return res.status(statusCode).end();
+    if (!expose) return res.status(statusCode).end();
     return res.status(statusCode).json({ statusCode, message, ...rest });
 };
 
